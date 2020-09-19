3870790343ebb779b9c19ccb017b4e34691a4152
scbushan05 <sc.bushan.05@gmail.com>
2020-09-20 01:15:10 +0530

    connected to mongodb using mongoose

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,11 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/bloggingapp', 
-{   
+mongoose.connect('mongodb://localhost:27017/bloggingapp', {   
     useNewUrlParser: true, 
     useUnifiedTopology: true
-}).then(() => {
-    console.log('connection successful');
-}).catch((error) => {
-    console.log('something wrong', error);
-})
+});
