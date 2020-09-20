1f9cc5f46edf6dc8e3e4cad60a6373e536f2eb3c
scbushan05 <sc.bushan.05@gmail.com>
2020-09-20 12:43:03 +0530

    created route to save the document

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 require('./db/mongoose');
 const express = require('express');
+const Blog = require('./models/blog');
 
 const app = express();
+app.use(express.json());
+
+app.post('/blogs', (req, res) => {
+    const blog = new Blog(req.body);
+    blog.save().then((blog) => {
+        res.status(201).send(blog);
+    }).catch((error) => {
+        res.status(400).send(error);
+    })
+})
 
 app.listen(3000, (req, res) => {
     console.log('app is running in port 3000!');
